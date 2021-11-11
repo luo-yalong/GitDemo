16e319766370e7cf90837fd81d093f011e3d32b2
luo-yalong <2192714220@qq.com>
2021-11-11 14:36:39 +0800

    控制器中添加查询用户姓名接口

--- MybatsPlusCrud/src/main/java/com/lyl/mybatsplus/controller/TestController.java
+++ MybatsPlusCrud/src/main/java/com/lyl/mybatsplus/controller/TestController.java
@@ -3,6 +3,9 @@ package com.lyl.mybatsplus.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author 罗亚龙
  * @date 2021/11/11 11:29
@@ -12,9 +15,19 @@ public class TestController {
 
     int id = 1;
 
+    Map<Integer,String > map = new HashMap<>();
+    {
+        map.put(1, "张三1");
+        map.put(2, "张三2");
+        map.put(3, "张三3");
+        map.put(4, "张三4");
+        map.put(5, "张三5");
+    }
+
+
     @GetMapping("/hello")
     public String hello(){
-        String str = "hello,world------";
+        String str = "hello,world----汉克僧--";
         System.out.println("str = " + str);
         return str;
     }
@@ -23,5 +36,17 @@ public class TestController {
     public int index(){
         return id ++;
     }
+
+    /**
+     * 通过用户id查询新消息
+     * @param id id
+     * @return 姓名
+     */
+    @GetMapping("/selectNameById")
+    public String selectNameById(int id){
+        return map.get(id);
+    }
+
+
 
 }
