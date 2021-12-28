704c543a7405b4b5b3158d158be7d113faf2b335
luo-yalong <2192714220@qq.com>
2021-12-28 20:35:10 +0800

    添加开发分支，添加l4~l7

---
+++ MybatsPlusCrud/src/main/java/com/lyl/mybatsplus/pojo/Product.java
@@ -0,0 +1,12 @@
+package com.lyl.mybatsplus.pojo;
+
+import lombok.Data;
+
+@Data
+public class Product {
+
+    private String l4;
+    private String l5;
+    private String l6;
+    private String l7;
+}
