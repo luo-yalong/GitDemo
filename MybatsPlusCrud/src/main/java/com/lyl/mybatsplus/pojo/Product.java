d5a5584496d2781fe529bacbdb54a27351c5bf48
luo-yalong <2192714220@qq.com>
2021-12-28 19:58:17 +0800

    添加Product文件

---
+++ MybatsPlusCrud/src/main/java/com/lyl/mybatsplus/pojo/Product.java
@@ -0,0 +1,13 @@
+package com.lyl.mybatsplus.pojo;
+
+import lombok.Data;
+
+@Data
+public class Product {
+
+    private String Id;
+    private String name;
+    private Integer l1;
+    private Integer l2;
+    private Integer l3;
+}
